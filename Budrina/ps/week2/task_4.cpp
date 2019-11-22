16edec9d3317e3b78d24a2826bcaef297004afbe
TanyaBud <57768870+TanyaBud@users.noreply.github.com>
2019-11-22 13:49:56 +0500

    Дом. работа
    Week2

---
+++ Budrina/ps/week2/task_4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int begin = 1;
+	int end = 20;
+	int i = begin;
+	cout << "Enter the digit from 0 to 20:";// Условие
+	cin >> a;
+
+	for (int i = 1; i <= 20; ++i) {
+		if (a > 20)continue; //если "a"  не будет соответствовать условию,
+
+		cout << "\n" << i * a << endl;// Правильный ответ
+	}
+
+	cout << endl; // то конец
+}
